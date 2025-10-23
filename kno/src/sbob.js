b778dd820693ef6ac01f4d9ef292342ad54527ec
nichePenguin <niche.penguin@gmail.com>
2025-10-23 23:02:57 +0300

    add depth and paging

--- kno/src/sbob.js
+++ kno/src/sbob.js
@@ -1,18 +1,82 @@
-var curr_page = 0;
-var items_per_page = 50;
+var curr_page = 1;
+var items_per_page = 48;
+var pages_total = 1;
+var items_total = items_per_page;
 var filter = {
   "contains": "",
 }
+var items = null;
 var last_search = "";
 var last_tags = "";
+var bugs_loading = true;
+var last_bug_width = 472;
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('only-bugs').addEventListener('change', handleTagChange);
   document.getElementById('no-spiders').addEventListener('change', handleTagChange);
   document.getElementById('include-cats').addEventListener('change', handleTagChange);
   document.getElementById('searchbar').addEventListener('input', handleSearchbarInput);
+  //document.querySelector('.grid').addEventListener('mouseover', handleItemHover);
   handleTagChange(null);
 });
+
+function handleItemMouseLeave(event) {
+  event.currentTarget.style.transform = '';
+}
+
+function handleItemHover(event) {
+  const item = event.currentTarget;
+  const rect = item.getBoundingClientRect()
+  const dx = event.clientX - rect.left - rect.width/2;
+  const dy = event.clientY - rect.top - rect.height/2;
+
+  item.style.transform = "rotateY("+(-dx)/16+"deg)" + "rotateX("+(dy)/20+"deg)" + "scale(1.1)"
+}
+
+function handlePageChange(event) {
+  if (bugs_loading) {
+    return
+  }
+  bugs_loading = true
+  const page = Math.max(1, int(event.currentTarget.innerText))
+  if (page == curr_page) {
+    bugs_loading = false
+    return;
+  }
+  curr_page = page;
+  loadBugs();
+}
+
+function adjustPageControl() {
+  if (items_total <= items_per_page) {
+    pages_total = 1;
+    document.getElementById('footer').style.display = 'none';
+    return;
+  } else {
+    pages_total = Math.ceil(items_total / items_per_page);
+    document.getElementById('footer').style.display = '';
+  }
+  const first = document.getElementById('page-first')
+  const left = document.getElementById('page-more-left')
+  const prev = document.getElementById('page-prev')
+  const curr = document.getElementById('page-curr')
+  const next = document.getElementById('page-next')
+  const right = document.getElementById('page-more-right')
+  const last = document.getElementById('page-last')
+
+  left.style.display = (curr_page <= 3) ? 'none' : '';
+  first.style.display = (curr_page <= 2) ? 'none' : '';
+  prev.style.display = (curr_page <= 1) ? 'none' : '';
+
+  right.style.display = (curr_page >= pages_total - 2) ? 'none' : '';
+  last.style.display = (curr_page >= pages_total - 1) ? 'none' : '';
+  next.style.display = (curr_page >= pages_total) ? 'none' : '';
+
+  prev.innerText = curr_page - 1
+  curr.innerText = curr_page
+  next.innerText = curr_page + 1
+  last.innerText = pages_total
+}
 
 function handleTagChange(event) {
   const only_bugs = document.getElementById('only-bugs').checked;
@@ -39,7 +103,8 @@ function handleTagChange(event) {
   const curr_tags = filter["tags"].sort().join(',');
   if (curr_tags != last_tags) {
     last_tags = curr_tags;
-    loadBugs();
+    curr_page = 1;
+    loadBugs()
   }
 }
 
@@ -50,10 +115,12 @@ function handleSearchbarInput(event) {
   }
   last_search = text;
   filter["contains"] = text;
+  curr_page = 1;
   loadBugs();
 }
 
 function loadBugs() {
+  bugs_loading = true;
   const data = JSON.stringify({
     "page": curr_page,
     "items_per_page": items_per_page ,
@@ -85,22 +152,35 @@ function load(json) {
   if (!json) return;
   const grid = document.querySelector('.grid');
   grid.innerHTML = '';
-  for (var i = 0; i < json["items"].length; i ++) {
-    const entry = json["items"][i];
+  items_total = json["total_count"];
+  items = json["items"]
+  for (var i = 0; i < items.length; i ++) {
+    const entry = items[i];
     const el = document.createElement('div');
-    el.className = 'item';
-    el.innerHTML = item(entry.name, entry.latin, entry.image);
+    const elems = document.getElementsByClassName('bug');
+    const bug_width = elems.length > 0 ? elems[0].offsetWidth : last_bug_width;
+    last_bug_width = bug_width
+    el.className = 'item-wrap';
+    el.setAttribute('index', i)
+    el.addEventListener('mousemove', handleItemHover);
+    el.addEventListener('mouseleave', handleItemMouseLeave);
+    el.innerHTML = item(entry.name, entry.latin, entry.image, bug_width);
     grid.appendChild(el);
   }
+  adjustPageControl()
+  bugs_loading = false
 }
 
-function item(name, latin, image) {
-  return `
+function item(name, latin, image, width) {
+  return `<div class="depth depth1"></div>
+          <div class="depth depth2"></div>
+          <div class="item">
           <img class="bug" src="${image}"><img>
           <div>
             <strong>${name}</strong>
             <br>
             <small>${latin}</small>
+          </div>
           </div>
 `;
 }
